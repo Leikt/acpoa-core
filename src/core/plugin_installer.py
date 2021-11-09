6892a9c101c759b2af244cee7f7f2c2dd826874c
leikt <leikt.solreihin@gmail.com>
2021-11-09 21:25:12 +0100

    Working repository management, package installation, custom pypi server
    Signed-off-by: leikt <leikt.solreihin@gmail.com>

--- src/core/plugin_installer.py
+++ src/core/plugin_installer.py
@@ -1,5 +1,6 @@
 import importlib.util
 import os
+import warnings
 
 from src.core.configuration import Configuration
 from src.core.singleton import Singleton
@@ -25,7 +26,12 @@ class PluginInstaller(metaclass=Singleton):
         :param package: name of the plugin to install
         :raise Exception: if the package couldn't be installed. Provides pip error code."""
 
-        result = None
+        if len(self._repositories) == 0:
+            raise Exception("There is no repository registered or enabled.")
+        if self.is_installed(package):
+            warnings.warn(Warning(f"Package '{package}' already installed."))
+            return
+
         for repo in self._repositories:
             result = repo.install(package)
             if result == 0: break
@@ -40,9 +46,13 @@ class PluginInstaller(metaclass=Singleton):
         """Uninstall the given package.
 
         :param package: name of the plugin to install
+        :raise ModuleNotFoundError: if the package isn't installed.
         :raise Exception: if the package couldn't be uninstalled. Provides pip error code."""
 
-        result = os.system(f"python3 -m pip -q uninstall -y {package}")
+        if not self.is_installed(package):
+            raise ModuleNotFoundError(f"Package '{package}' is not installed.")
+
+        result = self.Repository.remove(package)
         if result > 0: raise Exception(f"Package '{package}' can't be removed. Pip returned error core : {result}")
 
         if self._plugins_config.has_section(package):
@@ -50,16 +60,27 @@ class PluginInstaller(metaclass=Singleton):
             self._plugins_config.save()
 
     def update(self, package: str):
-        """Update the plugin. It needs to be installed to be updated.
+        """Update the plugin. It needs to be installed to be updated. It needs to be restarted to
+        take upgrade into account.
 
         :param package: name of the package to update
-        :raise Exception: if package couldn't be updated or if the package isn't installed."""
+        :raise Exception: if package couldn't be updated."""
 
-        if importlib.util.find_spec(package) is None:
-            raise Exception(f"Package '{package}' is not installed therefore it can't be freshened.")
+        # if importlib.util.find_spec(package) is None:
+        #     raise Exception(f"Package '{package}' is not installed therefore it can't be updated.")
+        if len(self._repositories) == 0:
+            raise Exception("There is no repository registered or enabled.")
 
-        result = os.system(f"python3 -m pip -q install --upgrade {package}")
+        for repo in self._repositories:
+            result = repo.upgrade(package)
         if result > 0: raise Exception(f"Package '{package}' can't be updated. Pip returned error core : {result}")
+
+    def is_installed(self, package: str) -> bool:
+        res = importlib.util.find_spec(package)
+        return res is not None
+
+    def is_enabled(self, package: str) -> bool:
+        pass
 
     def add_repository(self, name: str, index: str, editable: [bool, str] = False):
         """Add the repository to the list where to search plugins.
@@ -71,13 +92,16 @@ class PluginInstaller(metaclass=Singleton):
 
         section = self._config.subsection(self._ACPOA_CFG_SECTION_REPO, name)
         if self._config.has_section(section):
-            raise Exception(f"Repository {name} already is registered. Use modify_repository to update its data.")
+            warnings.warn(Warning(f"Repository {name} already is registered."))
+            return
 
         self._config.add_section(section)
         self._config.set(section, 'enabled', self._repo_eoi)
         self._config.set(section, 'index', index)
         self._config.setboolean(section, 'editable', editable)
         self._config.save()
+
+        self._repositories = self._load_repositories()
 
     def has_repository(self, name) -> bool:
         """Test if the given repository is installed.
@@ -88,6 +112,11 @@ class PluginInstaller(metaclass=Singleton):
         section = self._config.subsection(self._ACPOA_CFG_SECTION_REPO, name)
         return self._config.has_section(section)
 
+    def has_enabled_repository(self, name) -> bool:
+        section = self._config.subsection(self._ACPOA_CFG_SECTION_REPO, name)
+        if not self._config.has_section(section): return False
+        return self._config.getboolean(section, 'enabled')
+
     def remove_repository(self, name: str):
         """Remove the repository from the list.
 
@@ -96,6 +125,8 @@ class PluginInstaller(metaclass=Singleton):
         section = self._config.subsection(self._ACPOA_CFG_SECTION_REPO, name)
         self._config.remove_section(section)
         self._config.save()
+
+        self._repositories = self._load_repositories()
 
     def _load_repositories(self):
         repos = []
@@ -113,12 +144,19 @@ class PluginInstaller(metaclass=Singleton):
             self._editable = editable
             self._editable_opt = '-e' if editable else ''
 
-        def install(self, package, upgrade: bool = False):
-            command = f"python3 -m pip -q install {self._editable_opt} " \
+        def install(self, package, upgrade: bool = False) -> int:
+            command = f"pip -q install {self._editable_opt} " \
                       f"{'--upgrade' if upgrade else ''} " \
+                      f"--no-cache-dir " \
                       f"--index-url {self._index} " \
                       f"{package} "
             return os.system(command)
 
         def upgrade(self, package):
             return self.install(package, upgrade=True)
+
+        @staticmethod
+        def remove(package) -> int:
+            result = int(os.system(f"pip -q uninstall -y {package}"))
+            os.system('pip -q cache purge')
+            return result
